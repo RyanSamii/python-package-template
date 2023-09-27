387f81be651b2cf8830067f80ae981b75d3908cc
Ryan S <90662533+RyanSamii@users.noreply.github.com>
2023-09-27 16:51:01 -0400

    Update lint.py
    fixed global_status issue

--- src/python_package/lint.py
+++ src/python_package/lint.py
@@ -5,7 +5,7 @@ from pylint import lint
 
 THRESHOLD = 9
 run = lint.Run(["hello_world.py"], do_exit=False)
-score = run.linter.stats["global_note"]
+score = run.linter.stats.global_note
 if score < THRESHOLD:
         print("Linter failed: Score < threshold value")
         sys.exit(1)
