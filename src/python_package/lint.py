730003a13de8560cc62ca109760775d1612f3623
Ryan Samii <Rsamii@conestogac.on.ca>
2023-09-27 16:33:14 -0400

    added lint.py to have a code linter

---
+++ src/python_package/lint.py
@@ -0,0 +1,12 @@
+# hello_world
+
+import sys
+from pylint import lint
+
+THRESHOLD = 9
+run = lint.Run(["hello_world.py"], do_exit=False)
+score = run.linter.stats["global_note"]
+if score < THRESHOLD:
+        print("Linter failed: Score < threshold value")
+        sys.exit(1)
+sys.exit(0)
